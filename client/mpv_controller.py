ba2bdd406e204a7c87393fed84d4f2476e0a91a4
MingSong <tohrucooldoragon@gmail.com>
2026-09-12 16:37:48 +0800

    mpv send n receive

--- client/mpv_controller.py
+++ client/mpv_controller.py
@@ -2,6 +2,30 @@ import subprocess
 import time
 import json
 
+def send_command(pipe, command_list):
+    request = json.dumps({"command": command_list}) + "\n"
+    pipe.write(request.encode("utf-8"))
+
+    while True:
+        response = pipe.readline()
+        response_data = json.loads(response.decode("utf-8"))
+
+        if"error" in response_data:
+            return response_data
+
+def play(pipe):
+    return send_command(pipe, ["set_property", "pause", False])
+
+def pause(pipe):
+    return send_command(pipe, ["set_property", "pause", True])
+
+def seek(pipe, seconds):
+    return send_command(pipe, ["set_property", "time-pos", seconds])
+
+def get_time_pos(pipe):
+    response = send_command(pipe, ["get_property", "time-pos"])
+    return response["data"]
+
 video_path = "D:/SyncPlayer/test.mp4"
 ipc_path = r"\\.\pipe\mpvsocket"
 
@@ -15,12 +39,11 @@ pipe = open(ipc_path, "r+b", buffering=0)
 
 print("mpv connected")
 
-command = json.dumps({"command":["set_property", "pause", True]}) + "\n"
-pipe.write(command.encode("utf-8"))
+play(pipe)
+time.sleep(3)
 
-response = pipe.readline()
-print(response)
+current_time = get_time_pos(pipe)
+print("Current position:", current_time)
 
-response_text = response.decode("utf-8")
-response_data = json.loads(response_text)
-print(response_data)
+pause(pipe)
+print("paused")
